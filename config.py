7b5fa4d4840c81b03f3bcace22d3ad1a22225bbf
Bkphere <163238356+Bkphere@users.noreply.github.com>
2024-09-20 10:38:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "25713846"))
 API_HASH = getenv("API_HASH", "ca57c019f37ab7bf0bd9caead4846c88")
-BOT_TOKEN = getenv("BOT_TOKEN", "7269627262:AAH6oVfd4ONGBkHr_N1hpYZ0OgMNVfO0mN8")
+BOT_TOKEN = getenv("BOT_TOKEN", "7269627262:AAH6oVfd4ON8")
 OWNER_ID = int(getenv("OWNER_ID", "6750546542"))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://tvvt680:xfiV6ktxfGG4IZk2@cluster0.athjl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
 LOG_GROUP = int(getenv("LOG_GROUP", "-1002458368001"))
 FORCESUB = getenv("FORCESUB", "funnyzilla")
 DEFAULT_SESSION = getenv("DEFAULT_SESSION", "") # fill this only if you dont want to force your subscriber to login by this they can use the old method of invite link and can extract from public without login
