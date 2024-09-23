39adaa9e9153c08f0110881e05c11cf84d2b0a87
Aasifansa <aasifansari67543@gmail.com>
2024-09-23 19:21:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "28745135"))
-API_HASH = getenv("API_HASH", "123e77214315877fe406e25f35afd23d")
+API_ID = int(getenv("API_ID", "27662412"))
+API_HASH = getenv("API_HASH", "4a695060ecd737dffa9e92c540b46ed2")
 BOT_TOKEN = getenv("BOT_TOKEN", "7574299453:AAH3V90TNu8Wn1a8xEuiVrto-NdIGWrEBD0")
-OWNER_ID = int(getenv("OWNER_ID", "6750546542"))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://tvvt680:xfiV6ktxfGG4IZk2@cluster0.athjl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-LOG_GROUP = int(getenv("LOG_GROUP", "-1002305334420"))
-FORCESUB = getenv("FORCESUB", "funnyzilla")
+OWNER_ID = int(getenv("OWNER_ID", "6847319298"))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://Anokha:dbLifeline@cluster0.fvmpg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = int(getenv("LOG_GROUP", "-1002220096661"))
+FORCESUB = getenv("FORCESUB", "Luckyhub2y637")
 DEFAULT_SESSION = getenv("DEFAULT_SESSION", "") # fill this only if you dont want to force your subscriber to login by this they can use the old method of invite link and can extract from public without login
